aba419cc1d50db41579fe740b177f41e3574889c
BatsAppDev <ad.stokes.25@gmail.com>
2023-12-02 17:26:58 +0200

    part 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿var output = File.ReadAllLines("input.txt")
+    .Select(line =>
+    {
+        var keyRes = line.Split(":");
+
+        return new
+        {
+            Id = int.Parse(keyRes.First().Split(" ").Last()),
+            Sets = keyRes.Last()
+                .Split(";", StringSplitOptions.TrimEntries)
+                .Select(game => game.Split(",", StringSplitOptions.TrimEntries)
+                    .Select(contentsString =>
+                    {
+                        var contents = contentsString.Split(" ");
+
+                        return new
+                        {
+                            Count = int.Parse(contents.First()),
+                            Color = contents.Last()
+                        };
+                    }))
+        };
+    })
+    .Where(line =>
+    {
+        var largestFound = new Dictionary<string, int>();
+
+        foreach (var sets in line.Sets)
+        {
+            foreach (var set in sets)
+            {
+                if (set.Count > largestFound.GetValueOrDefault(set.Color))
+                    largestFound[set.Color] = set.Count;
+            }
+        }
+        
+        return
+            largestFound.GetValueOrDefault("red") <= 12 &&
+            largestFound.GetValueOrDefault("green") <= 13 &&
+            largestFound.GetValueOrDefault("blue") <= 14;
+    })
+    .Select(game => game.Id)
+    .Sum();
+    
+Console.WriteLine(output);
